13a8a208cb1c94256b71dd5037df1427f124820f
Bogdan <53011638+bodiabeliy@users.noreply.github.com>
2021-04-08 22:23:35 +0300

    smart part1

---
+++ index.js
@@ -0,0 +1,17 @@
+const header = document.getElementsByTagName('header')
+
+
+const about =  document.getElementById('about')
+header.innerHTML = `
+<div class="sectiption">
+ <h1 class="center"><span class="orange">Smart</span> Agency Landign Page</h1>
+ <p class="center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, sunt et? 
+  Placeat nam enim quaerat tempore iure commodi corrupti, praesentium facere, eos quae deserunt consequatur officia voluptate?</p>
+</div>
+
+`
+
+const office = document.getElementById('office')
+office.innerHTML = `
+
+`
